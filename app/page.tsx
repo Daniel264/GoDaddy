9820f25bffd5c51b9c9eef185c5ddaebb86b7d64
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-17 23:49:33 +0100

    Design the inputBox component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import InputBox from "@/components/InputBox";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <InputBox />
     </>
   );
 }
--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -4,20 +4,26 @@ import Image from "next/image";
 
 const InputBox = () => {
   return (
-    <>
-      <div className="flex flex-row">
-        <input type="text" className="max-w-[100px]:" />
-        <button className="btn btn-primary ml-2">search Domain</button>
+    <div className="flex p-10">
+      <div className="flex flex-row pr-5">
+        <input
+          type="text"
+          placeholder="Type the domain you want"
+          className="w-[850px] bg-[#dfdfdf] outline-none placeholder:text-brown px-5"
+        />
+        <button className=" btn bg-[#00838C] h-[67px] rounded-none text-white">
+          search Domain
+        </button>
       </div>
-      <div>
-        <p className="font-semibold text-[#888]">.online $0.99*</p>
-        <p className="font-bold text-[#888]">.net $14.99*</p>
-        <p>
-          <Image src={inc} alt="inc logo" /> $999.99*
+      <div className="flex pt-5">
+        <p className="font-semibold text-sm text-[#888] pr-3">.online $0.99*</p>
+        <p className="font-bold text-sm text-[#888] pr-3">.net $9.99</p>
+        <p className="text-sm text-[#888] flex pr-3">
+          <Image className="w-7 h-7" src={inc} alt="inc logo" /> $999.99*
         </p>
-        <p className="font-semibold text-[#888]">.online $11.99*</p>
+        <p className="font-semibold text-sm text-[#888]">.online $11.99*</p>
       </div>
-    </>
+    </div>
   );
 };
 
