431798aba70b4baf45fd79b7fd18e7ffce609d06
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-18 00:15:47 +0100

    make the inputBox responsive

--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -4,21 +4,21 @@ import Image from "next/image";
 
 const InputBox = () => {
   return (
-    <div className="flex p-10">
+    <div className="flex flex-col md:flex-row p-10">
       <div className="flex flex-row pr-5">
         <input
           type="text"
           placeholder="Type the domain you want"
-          className="w-[850px] bg-[#dfdfdf] outline-none placeholder:text-brown px-5"
+          className="w-full md:w-[850px] bg-[#dfdfdf] outline-none placeholder:text-brown px-5"
         />
-        <button className=" btn bg-[#00838C] h-[67px] rounded-none text-white">
+        <button className=" btn bg-[#00838C] h-[67px] rounded-none text-white ">
           search Domain
         </button>
       </div>
       <div className="flex pt-5">
         <p className="font-semibold text-sm text-[#888] pr-3">.online $0.99*</p>
         <p className="font-bold text-sm text-[#888] pr-3">.net $9.99</p>
-        <p className="text-sm text-[#888] flex pr-3">
+        <p className="text-sm text-[#888] md:flex pr-3">
           <Image className="w-7 h-7" src={inc} alt="inc logo" /> $999.99*
         </p>
         <p className="font-semibold text-sm text-[#888]">.online $11.99*</p>
