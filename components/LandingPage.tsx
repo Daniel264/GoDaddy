0d49e55713b500788ad0c727689c2535abf54c6a
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-18 22:39:53 +0100

    change the hover color of the button

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -11,11 +11,11 @@ const LandingPage = () => {
             <p className="font-semibold pb-5">Websites & Commerce</p>
             <h1 className="font-bold
             text-3xl lg:text-5xl pb-5">Create your <br /> website</h1>
-            <button className="btn bg-black text-white border-none">Start for free</button>
+            <button className="btn bg-black text-white border-none hover:hover:bg-[#555]">Start for free</button>
             <p className="pt-7">No charge, no obligation, nothing to cancel.**</p>
           </div>
 
-          <Image className="h-[98%] rounded-md " src={firstImg} alt={""} />
+          <Image className="h-[100%] rounded-md " src={firstImg} alt={""} />
         </div>
         <div className=" bg-sky-50 rounded-md pl-14 pt-16 pr-10 md:pr-32">
           <p className="font-semibold pb-5">Domain Names</p>
@@ -23,7 +23,7 @@ const LandingPage = () => {
             Grab a .com for just $0.01* /1st yr
           </h1>
           <p className="pb-3 font-semibold">2-year purchase required*</p>
-          <button className="btn bg-black text-white">Find Your Domain</button>
+          <button className="btn bg-black text-white hover:bg-[#555]">Find Your Domain</button>
           <p className="pt-5 font-semibold">Transfer Your Domains</p>
           <p className="pt-3">
             Domains include free privacy protection forever*
