e1828e64a0517e5a46009bf6bef40a1bf94fec46
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-18 21:28:08 +0100

    Make the LandingPage responsive

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const LandingPage = () => {
   return (
     <>
-      <div className="grid grid-cols-[63%_37%] gap-3 w-[100%] h-[600px] p-10">
+      <div className="grid grid-rows-2 grid-cols-1 lg:grid lg:grid-cols-[63%_37%] lg:gap-3 lg:w-[100%] h-[1000px] p-10">
         <div className="bg-purple-50">Box 1</div>
         <div className=" bg-sky-50">Box 2</div>
       </div>
