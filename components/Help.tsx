d33c6750edc8616e2e64bfc2329c2fa749bc6c78
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-20 20:59:05 +0100

    Adjust responsiveness in help page

--- components/Help.tsx
+++ components/Help.tsx
@@ -4,18 +4,26 @@ import Image from "next/image";
 
 const Help = () => {
   return (
-    <div className=" text-center">
+    <div className=" text-center lg:text-left">
       <div className="bg-[#fffada] h-fit sm:flex sm:h-fit sm:py-10 md:items-center">
         <div className="px-10">
-          <Image src={moon} alt="image of moon" className="h-[300px] md:w-[800px] md:h-[500px]" />
+          <Image
+            src={moon}
+            alt="image of moon"
+            className="h-[300px] md:w-[800px] md:h-[500px]"
+          />
         </div>
-        <div className="px-10">
-          <h2 className="font-semibold text-3xl md:text-5xl md:pb-7">Why go with GoDaddy?</h2>
-          <p className="pb-5">
-            Because we know that even the best technology is only as good as the
-            people behind it. That’s why we offer expert support, plus a lot
-            more.
-          </p>
+        <div className="px-10 lg:pr-32">
+          <h2 className="font-semibold text-3xl md:text-5xl md:pb-7">
+            Why go with GoDaddy?
+          </h2>
+          <div className="lg:w-[500px]">
+              <p className="pb-5 ">
+                Because we know that even the best technology is only as good as the
+                people behind it. That’s why we offer expert support, plus a lot
+                more.
+              </p>
+          </div>
           <button className="btn bg-white text-black border-black">
             Get Help
           </button>
