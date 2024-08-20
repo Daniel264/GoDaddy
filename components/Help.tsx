40bd88bfa2930e6a014c8790fefab792041759d4
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-20 20:28:09 +0100

    make the help page responsive

--- components/Help.tsx
+++ components/Help.tsx
@@ -5,12 +5,12 @@ import Image from "next/image";
 const Help = () => {
   return (
     <div className=" text-center">
-      <div className="bg-[#fffada] h-[550px]">
+      <div className="bg-[#fffada] h-fit sm:flex sm:h-fit sm:py-10 md:items-center">
         <div className="px-10">
-          <Image src={moon} alt="image of moon" className="h-[300px]" />
+          <Image src={moon} alt="image of moon" className="h-[300px] md:w-[800px] md:h-[500px]" />
         </div>
         <div className="px-10">
-          <h2 className="font-semibold text-3xl">Why go with GoDaddy?</h2>
+          <h2 className="font-semibold text-3xl md:text-5xl md:pb-7">Why go with GoDaddy?</h2>
           <p className="pb-5">
             Because we know that even the best technology is only as good as the
             people behind it. That’s why we offer expert support, plus a lot
