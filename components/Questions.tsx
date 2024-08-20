e3666156e8b0f93a1af4e3c998e4cd6dd1706c6b
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-20 18:11:37 +0100

    make the accordion responsive

--- components/Questions.tsx
+++ components/Questions.tsx
@@ -2,38 +2,152 @@ import React from "react";
 
 const Questions = () => {
   return (
-    <div className="text-center w-screen h-fit px-40 py-40">
+    <div className="text-center w-screen h-fit px-10 py-40 md:px-96">
       <h1 className="font-medium text-4xl">
         Millions of customers rely on our domains and web hosting to get their
         ideas online
       </h1>
       <h2 className="text-3xl py-8">Frequently Asked Questions</h2>
-      <div className="join join-vertical w-full">
+      <div className="join join-vertical w-full ">
         <div className="collapse collapse-arrow join-item border-base-300 border">
           <input type="radio" name="my-accordion-4" defaultChecked />
-          <div className="collapse-title text-xl font-medium">
-            Click to open this one and close others
+          <div className="collapse-title text-xl font-medium ">
+            <p className="text-3xl">
+              How does GoDaddy help small business owners succeed?
+            </p>
           </div>
           <div className="collapse-content">
-            <p>hello</p>
+            <p>
+              GoDaddy offers more than just a platform to build your website, we
+              offer everything you need to create an effective, memorable online
+              presence. Already have a site? We offer hosting plans that will
+              keep it fast, secure and online. Our professional email helps you
+              build a professional image, while our online marketing tools
+              empower entrepreneurs to get online with an SEO-friendly website.
+              GoDaddy is an all-in-one solution provider to get your idea
+              online, backed with expert, personalized support from GoDaddy
+              Guides.
+            </p>
           </div>
         </div>
         <div className="collapse collapse-arrow join-item border-base-300 border">
           <input type="radio" name="my-accordion-4" />
           <div className="collapse-title text-xl font-medium">
-            Click to open this one and close others
+            <p className="text-3xl">Why do I need a website for my business?</p>
           </div>
           <div className="collapse-content">
-            <p>hello</p>
+            <p>
+              Even small local businesses that only serve their hometown need a
+              site. Thats because the web is the first place people go when
+              looking for a product or service. A website helps every business:
+              <ul>
+                <li>Promote and sell their products and services</li>
+                <li>
+                  Connect with new customers (and keep existing customers)
+                </li>
+                <li>Build credibility</li>
+                <li>
+                  Compete with bigger businesses Control their brand and keep
+                  their marketing fresh and current
+                </li>
+              </ul>
+              Best of all, a website is much easier and more affordable than you
+              might think. GoDaddy offers a complete selection of online tools
+              for building websites, along with hosting, email and marketing
+              options to grow their business on the web.
+            </p>
           </div>
         </div>
         <div className="collapse collapse-arrow join-item border-base-300 border">
           <input type="radio" name="my-accordion-4" />
           <div className="collapse-title text-xl font-medium">
-            Click to open this one and close others
+            <p className="text-3xl">Why do I need a professional email?</p>
           </div>
           <div className="collapse-content">
-            <p>hello</p>
+            <p>
+              There are a lot of reasons to switch from your free email to a
+              domain-based email, but here are the top 3:
+              <ul>
+                <li>
+                  Customers are much more likely to trust a professional address
+                  like lily@lilysbikes.com than lilysbikestexas84@notmail.com.
+                </li>
+                <li>
+                  Professional Email provides world-class security with
+                  industry-leading spam and virus filters.
+                </li>
+                <li>
+                  Personalized email addresses promote your business more
+                  effectively. Every time you send an email, you are giving your
+                  web address to customers and prospects, encouraging them to
+                  visit your website.
+                </li>
+              </ul>
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow join-item border-base-300 border">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-xl font-medium">
+            <p className="text-3xl">Why get a domain name from us</p>
+          </div>
+          <div className="collapse-content">
+            <p>
+              GoDaddy is the world’s largest and trusted domain registrar that
+              empowers people like you with creative ideas to succeed online.
+              Buying a domain name is easy with our domain search tool and
+              domain name generator tools you can find the perfect website
+              address for your business.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow join-item border-base-300 border">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-xl font-medium">
+            <p className="text-3xl">Why use GoDaddy Website Builder?</p>
+          </div>
+          <div className="collapse-content">
+            <p>
+              Because it’s a fast, simple way to create a website that you — and
+              your customers — will love. Simply type in your idea or industry
+              and GoDaddy Website Builder will pull up a number of
+              professionally designed, ready-to-launch templates. From there,
+              just add your own text and images and you’re good to go.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow join-item border-base-300 border">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-xl font-medium">
+            <p className="text-3xl">
+              What makes GoDaddy Web Hosting the world leader?
+            </p>
+          </div>
+          <div className="collapse-content">
+            <p>
+              With nearly 20 years in the industry, we have the experience, the
+              technology and the hosting experts to help web designers,
+              developers, bloggers and online businesses create and maintain
+              their online presence. Our secure web hosting platform, complete
+              with cPanel, offers a 99.9% uptime guarantee*** and award-winning
+              support, which has helped nearly 20+ million customers get online.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow join-item border-base-300 border">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-xl font-medium">
+            <p className="text-3xl">Why choose GoDaddy for WordPress?</p>
+          </div>
+          <div className="collapse-content">
+            <p>
+              GoDaddy offers reliable and affordable WordPress hosting and
+              WooCommerce hosting plans, one-click installs and the latest
+              version so you have the most recent features available for your
+              site. Your website content is managed from one place; you have a
+              wealth of themes and add-ons giving you limitless options to
+              succeed.
+            </p>
           </div>
         </div>
       </div>
