95af63749338da04c7205352d4afca6a15742d04
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-20 21:03:54 +0100

    Adjust texts in questions page

--- components/Questions.tsx
+++ components/Questions.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const Questions = () => {
   return (
     <div className="text-center w-screen h-fit px-10 py-40 md:px-48 ">
-      <h1 className="font-medium text-4xl">
+      <h1 className="font-semibold text-4xl lg:px-72">
         Millions of customers rely on our domains and web hosting to get their
         ideas online
       </h1>
-      <h2 className="text-3xl py-8">Frequently Asked Questions</h2>
+      <h2 className="text-3xl font-medium py-8">Frequently Asked Questions</h2>
       <div className="join join-vertical w-full ">
         <div className="collapse collapse-arrow join-item border-base-300 border">
           <input type="radio" name="my-accordion-4" defaultChecked />
