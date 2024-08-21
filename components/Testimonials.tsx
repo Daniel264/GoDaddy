f20ced2c028867099547f0c1798aeb164ab5f2f7
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-21 14:09:35 +0100

    Style ratings properly

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -3,8 +3,8 @@ import Text from "./TestimonialText";
 
 const Testimonials = () => {
   return (
-    <div className="px-10 pt-20">
-      <div className="">
+    <div className=" pt-20">
+      <div>
         <div>
           <h1 className="font-semibold text-center md:text-left text-3xl lg:text-5xl">
             Trusted by 20+ million customers around the world{" "}
@@ -15,32 +15,41 @@ const Testimonials = () => {
             <div key={item.id} className="carousel-item max-w-xs">
               <div className="p-4 bg-white rounded-box shadow-md">
                 <div className="rating mb-10">
-                  <input
-                    type="radio"
-                    name="rating-4"
-                    className="mask mask-star-2 bg-green-500"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-4"
-                    className="mask mask-star-2 bg-green-500"
-                    defaultChecked
-                  />
-                  <input
-                    type="radio"
-                    name="rating-4"
-                    className="mask mask-star-2 bg-green-500"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-4"
-                    className="mask mask-star-2 bg-green-500"
-                  />
-                  <input
-                    type="radio"
-                    name="rating-4"
-                    className="mask mask-star-2 bg-green-500"
-                  />
+                  <div className="bg-green-500 mr-1 pt-1">
+                    <input
+                      type="radio"
+                      name="rating-4"
+                      className="mask mask-star-2 bg-white"
+                    />
+                  </div>
+                  <div className="bg-green-500 mr-1 pt-1">
+                    <input
+                      type="radio"
+                      name="rating-4"
+                      className="mask mask-star-2 bg-white"
+                    />
+                  </div>
+                  <div className="bg-green-500 mr-1 pt-1">
+                    <input
+                      type="radio"
+                      name="rating-4"
+                      className="mask mask-star-2 bg-white"
+                    />
+                  </div>
+                  <div className="bg-green-500 mr-1 pt-1">
+                    <input
+                      type="radio"
+                      name="rating-4"
+                      className="mask mask-star-2 bg-white"
+                    />
+                  </div>
+                  <div className="bg-green-500 mr-1 pt-1">
+                    <input
+                      type="radio"
+                      name="rating-4"
+                      className="mask mask-star-2 bg-white"
+                    />
+                  </div>
                 </div>
                 <p className="text-lg text-black  mb-2">{item.text}</p>
                 <p className="mt-10">{item.author}</p>
