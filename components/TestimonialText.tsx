a0fe9a9777b41b39122073b2f43fe278bc8420b8
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-21 14:21:37 +0100

    Add more dummy text to testimonials

--- components/TestimonialText.tsx
+++ components/TestimonialText.tsx
@@ -1,20 +1,29 @@
 const Text = [
   {
     id: 1,
+    header: "ONE-STOP AWESOMENESS FOR BUSINESS",
     text: "We use GoDaddy for our business website as well as commerce/payment processing. We get lots of complements on our website which we built in about an hour using a GoDaddy template. We also use their commerce payment processing and it is the BEST! Lowest fees, no per-transaction fee, and the Poynt terminal is so easy to use and dependable.",
     author: "Shannon Timmons",
   },
 
   {
     id: 2,
+    header: "GODADDY IS THE BEST!",
     text: "GoDaddy is my favorite business to purchase and host domains. There customer support is excellent, and all of their products and services are super easy to use. I highly recommend GoDaddy",
     author: "Happy customer",
   },
 
   {
     id: 3,
-    text: "GoDaddy has made creating a beautiful & professional looking website so easy! The email setup was quick too. Anytime i've had a question or even onnce a technical s=issue they have solved it in minutes! I have been so impressed with the helpfulness i've received since switching to GoDaddy! I cannot recommend them enough!",
+    header: "GODADDY HAS MADE EVERYTHING SO EASY!!!",
+    text: "GoDaddy has made creating a beautiful & professional looking website so easy! The email setup was quick too. Anytime i've had a question or even once a technical s=issue they have solved it in minutes! I have been so impressed with the helpfulness i've received since switching to GoDaddy! I cannot recommend them enough!",
     author: "Crystal H",
+  },
+  {
+    id: 4,
+    header: "GREAT APPROACH FOR THE SMALL BUSINESS",
+    text: "Great approach for the small business. Nicely packaged to get email and website up and running quickly",
+    author: "Kalpesh Mistry",
   },
 ];
 
