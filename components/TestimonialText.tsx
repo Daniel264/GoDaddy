a54a22fe0e1ecac922ad99b7c20d320835579f29
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-21 13:18:32 +0100

    Create objects for storing data data

---
+++ components/TestimonialText.tsx
@@ -0,0 +1,21 @@
+const Text = [
+  {
+    id: 1,
+    text: "We use GoDaddy for our business website as well as commerce/payment processing. We get lots of complements on our website which we built in about an hour using a GoDaddy template. We also use their commerce payment processing and it is the BEST! Lowest fees, no per-transaction fee, and the Poynt terminal is so easy to use and dependable.",
+    author: "Shannon Timmons",
+  },
+
+  {
+    id: 2,
+    text: "GoDaddy is my favorite business to purchase and host domains. There customer support is excellent, and all of their products and services are super easy to use. I highly recommend GoDaddy",
+    author: "Happy customer",
+  },
+
+  {
+    id: 3,
+    text: "GoDaddy has made creating a beautiful & professional looking website so easy! The email setup was quick too. Anytime i've had a question or even onnce a technical s=issue they have solved it in minutes! I have been so impressed with the helpfulness i've received since switching to GoDaddy! I cannot recommend them enough!",
+    author: "Crystal H",
+  },
+];
+
+export default Text;
--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Testimonials = () => {
   return (
-    <div>
-        <h1>Trusted by </h1>
-    </div>
-  )
-}
+    <>
+      <div>
+        <h1 className="font-semibold text-left text-5xl">
+          Trusted by 20+ million customers around the world{" "}
+        </h1>
+      </div>
+      <div className="flex flex-col lg:flex-row">
+        <div>
+          <p></p>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
