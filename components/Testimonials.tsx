4fec6a922d6f244e17242093cdb3a349657e3bff
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-08-23 15:22:03 +0100

    remove width bug in testimonials

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -5,14 +5,14 @@ const Testimonials = () => {
   return (
     <div className=" pt-20 px-10">
       <div>
-        <div className="ml-10">
-          <h1 className="font-semibold text-center md:text-left text-3xl lg:text-5xl">
+        <div className="">
+          <h1 className="font-semibold text-left md:text-left text-3xl lg:text-5xl">
             Trusted by 20+ million <br /> customers around the world
           </h1>
         </div>
-        <div className="carousel carousel-center rounded-box max-w-md space-x-4 sm:max-w-none py-4 md:w-full">
+        <div className="carousel carousel-center rounded-box max-w-xs space-x-4 sm:max-w-none py-4 md:w-full">
           {Text.map((item) => (
-            <div key={item.id} className="carousel-item max-w-xs md:max-w-[490px]">
+            <div key={item.id} className="carousel-item max-w-[18rem] md:max-w-[490px]">
               <div className="p-4 bg-gray-100 rounded-box shadow-md">
                 <p className="mb-5 text-black font-semibold">{item.header}</p>
                 <div className="rating mb-10">
@@ -53,7 +53,7 @@ const Testimonials = () => {
                   </div>
                 </div>
 
-                <p className="text-lg text-black font-semibold  mb-2">
+                <p className="text-base md:text-lg text-black font-medium  mb-2">
                   {item.text}
                 </p>
                 <p className=" mt-10">{item.author}</p>
